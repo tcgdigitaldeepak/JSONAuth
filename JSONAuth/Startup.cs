004600994ce915693390ae54307e1ecc76fe5754
Deepak <tcgdigital.deepak@gmail.com>
2018-01-17 11:11:59 +0530

    swaggerJWT

--- JSONAuth/Startup.cs
+++ JSONAuth/Startup.cs
@@ -39,26 +39,10 @@ namespace JSONAuth
                         options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
                         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
                     })
-                    .AddJwtBearer(cfg =>
-                    {
-                        cfg.RequireHttpsMetadata = false;
-                        cfg.SaveToken = true;
-                        cfg.TokenValidationParameters = new TokenValidationParameters
-                        {
-                            ValidateIssuerSigningKey = true,
-                            ValidIssuer = Configuration.GetSection("JwtToken").GetSection("JwtIssuer").Value,
-                            ValidateAudience = false,
-                            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(Configuration.GetSection("JwtToken").GetSection("JwtKey").Value)),
-                            ClockSkew = TimeSpan.Zero
-                        };
-                    });
+                    .AddJwtBearer();
 
             services.AddMvc();
-            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "jWt Auth API", Version = "v1" }));
-            services.AddSwaggerGen(c =>
-            {
-                c.AddSecurityDefinition("Bearer", new ApiKeyScheme() { In = "header", Description = "Please insert JWT with Bearer into field", Name = "Authorization", Type = "apiKey" });
-            });
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -83,9 +67,8 @@ namespace JSONAuth
                     template: "{controller}/{action=Index}/{id?}");
             });
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "jWt Auth Api"));
-         
+            app.UseSwaggerDocumentation();
+                     
         }
     }
 }
---
+++ JSONAuth/SwaggerServiceExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace JSONAuth
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new Info { Title = "Main API v1.0", Version = "v1.0" });
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Versioned API v1.0");
+
+                c.DocExpansion("none");
+            });
+
+            return app;
+        }
+    }
+}
